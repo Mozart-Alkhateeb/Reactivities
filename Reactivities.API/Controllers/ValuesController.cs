d4a7d4f78b71ea3bcfdf0179c6638271bf3f79a5
Mozart Al Khateeb <mozart.alkhateeb@gmail.com>
2020-01-12 22:42:04 +0200

    Initial Commit

---
+++ Reactivities.API/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Reactivities.Domain;
+using Reactivities.Persistence;
+
+namespace Reactivities.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger<ValuesController> _logger;
+        private readonly AppDbContext _context;
+
+        public ValuesController(ILogger<ValuesController> logger, AppDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Value>>> Get()
+        {
+            var res = await _context.Values.ToListAsync();
+            return Ok(res);
+        }        
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Value>> Get(int id)
+        {
+            var res = await _context.Values.FindAsync(id);
+            return Ok(res);
+        }
+    }
+}
